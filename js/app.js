795ab4f3342e33fb97b2fa8f8074e5eadcc5ffc6
Hema Pillay <hemapillay@Hemas-Air.hsd1.ca.comcast.net>
2016-10-05 18:02:06 -0700

    Added components such as off canvas, abide validation in forms

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+$(function() {
+	$('#gallery a').on('click', function() {
+		var image = $(this).find('img').attr('src');
+		var fullPath = image.replace('thumbnail', 'fullsize');
+		$('#galleryModal #imageContainer').attr('src', fullPath);
+	});
+});
+
 $(document).foundation();
